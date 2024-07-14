576449eacda90aba0664c66c93aeb62269e50aad
Dan Paxton <danpaxton@Mac-Air.local>
2024-07-14 08:48:14 -0400

    test build

--- server/Server.js
+++ server/Server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://notesite-murex.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
