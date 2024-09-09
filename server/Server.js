9e486be1bb63d90ed7edde14e4c1d0e58128f21b
Dan Paxton <danpaxton@vl965-172-31-75-255.wireless.umass.edu>
2024-09-09 15:09:34 -0400

    changes

--- server/Server.js
+++ server/Server.js
@@ -4,13 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors({
-  origin: [
-    "https://notesite-delta.vercel.app",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(require("./routes/user"));
